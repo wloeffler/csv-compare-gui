de3294e78417500b92aa4c24b24231d745110332
wloeffler <will@wloeffler.com>
2020-07-31 22:23:20 -0400

    this is intial commit

---
+++ Info.py
@@ -0,0 +1,9 @@
+"""
+This class is used to handle the inforation entered in the UI and pass it to the processing class
+"""
+
+class inputInfo:
+
+    def __init__(self, fileNameA, fileNameB):
+        self.fileNameA = fileNameA
+        self.fileNameB = fileNameB
